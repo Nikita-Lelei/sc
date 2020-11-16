07a1b3fad8948d144b3685ff35d028f117a93b9a
Никита Лелей <hallawandalla@gmail.com>
2020-11-16 14:23:09 +0300

    prime numbers

--- html_1/script.js
+++ html_1/script.js
@@ -1,44 +1,114 @@
-async function crashScreen__await() {
-var img = document.createElement('img');
-img.src = await imgAfterSomeTime();
-img.className = "crash-image";
-document.body.append(img);
-await deleteImageAfterSomeTime(img);
+var numberArray = [];
+var primeNumbers = [];
 
-  }
-
-  function imgAfterSomeTime(){
-    return new Promise(resolve=> {
-        setTimeout(() => {
-            resolve('1.png');
-        }, 3000);
-    } )
-  }
-
-  function deleteImageAfterSomeTime(img) {
-    return new Promise(resolve=> {
-setTimeout(() => {
-    img.remove();
-    resolve();
-}, 3000);
-    } )
-  }
-
-async function crashScreen__then() {
-
-    new Promise(function (resolve, reject) {
-        setTimeout(() => resolve('1.png'), 3000);
-    }).then(function (imageSrc) {
-        var img = document.createElement('img');
-        img.src = imageSrc;
-        img.className = "crash-image";
-        document.body.append(img);
-        return img;
-    }).then(function (img) {
-        setTimeout(() => img.remove(), 3000)
-    });
+function createArray(size) {
+    for (let i = 0; i <= size; i++) {
+        numberArray[i] = i;
+    }
+    primeNumbers = numberArray.slice();
 
 }
 
-//вывести граф в канвас
+function createTable(size) {
+    createArray(size);
+    var sizeSqrt = Math.sqrt(size);
+    var raws = Math.ceil(sizeSqrt);
+    var columns = Math.round(sizeSqrt)
+    var tableRef = document.getElementById("result");
+    var newRow;
+    var newCell;
+    var newText;
+    var count = 1;
+    for (let i = 0; i < raws; i++) {
+        newRow = tableRef.insertRow();
+        for (let j = 0; j < columns; j++) {
+            newCell = newRow.insertCell();
+            if (count <= size) {
+                newText = document.createTextNode(count);
+                newCell.appendChild(newText);
+            }
+            count++;
+        }
+    }
+    eratosfen();
+}
+
+function deleteNumber(arr, number) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == number) {
+            arr.splice(i, 1)
+        }
+    }
+}
+
+async function eratosfen() {
+    for (let i = 2; Math.pow(primeNumbers[i], 2) <= numberArray.length; i++) {
+        await findPrimeNumber(primeNumbers[i])
+    }
+}
+
+var changes_ = new Set();
+var changes = [];
+var styleCount = 0;
+
+async function changeBG(arr) {
+    return new Promise((resolve, reject) => {
+        var index = 0;
+        var result = document.getElementById("result").getElementsByTagName("td");
+        function useStyle() {
+            if (index < arr.length && arr.length !== null) {
+                if (result[arr[index]].style.background == "") {
+                    switch (styleCount) {
+                        case 1: result[arr[index]].style.background = "red"; break;
+                        case 2: result[arr[index]].style.background = "green"; break;
+                        case 3: result[arr[index]].style.background = "navy"; break;
+                        case 4: result[arr[index]].style.background = "orange"; break;
+                        case 5: result[arr[index]].style.background = "purple"; break;
+                        case 6: result[arr[index]].style.background = "pink"; break;
+                        case 7: result[arr[index]].style.background = "blue"; break;
+                        default: result[arr[index]].style.background = "yellow"; break;
+                    }
+                }
+                index++;
+            } else {
+                clearInterval(q);
+                resolve(0);
+            }
+        }
+        styleCount++;
+        var q = setInterval(useStyle, 100);
+    })
+
+}
+
+async function findPrimeNumber(p) {
+
+    for (let i = Math.pow(p, 2); i < numberArray.length; i += p) {
+        changes_.add(i - 1)
+        deleteNumber(primeNumbers, i);
+    }
+    changes = Array.from(changes_);
+    changes_.clear();
+    await changeBG(changes);
+    changes = [];
+}
+function refresh() {
+    document.getElementById("result").innerHTML = "";
+    styleCount = 0;
+    changes_.clear();
+    changes = [];
+     numberArray = [];
+primeNumbers = [];
+}
+
+function completeTask() {
+    refresh();
+    var input = +prompt("enter number (100k max):");
+    if (typeof input === 'number' && isFinite(input) && input <=100000) {
+        createTable(input);
+    } else {
+        alert("incorrect value");
+        completeTask();
+    }
+}
 
